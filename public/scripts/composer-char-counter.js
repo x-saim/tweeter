b83f72f147f9893954d9c73c3da22fa30b2a7e4d
Saim Amir <saimxamir@gmail.com>
2023-04-18 07:28:52 -0400

    add blur, keyup, keydown, keypress, input events

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
   console.log("ready");
 
+  $(".new-tweet textarea").on("blur", (event) => {
+    console.log(event.type)
+  });
   $(".new-tweet textarea").on("keyup", (event) => {
     console.log(event.type)
-  })
-  
+  });
+
+  $(".new-tweet textarea").on("keydown", (event) => {
+    console.log(event.type)
+  });
+
+  $(".new-tweet textarea").on("keypress", (event) => {
+    console.log(event.type)
+  });
+
+  $(".new-tweet textarea").on("input", (event) => {
+    console.log(event.type)
+  });
+
 });
