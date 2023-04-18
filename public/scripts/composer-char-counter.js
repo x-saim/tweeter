1710461f943718d4f935cac25866c954dac3607d
Saim Amir <saimxamir@gmail.com>
2023-04-18 07:55:06 -0400

    use jquery for this.val() to access the length fo the value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
   console.log("ready");
 
-  $(".new-tweet textarea").on("blur", (event) => {
-    console.log(event.type)
-  });
-  $(".new-tweet textarea").on("keyup", (event) => {
-    console.log(event.type)
+  $(".new-tweet textarea").on("input", function() {
+    console.log(this)
   });
 
-  $(".new-tweet textarea").on("keydown", (event) => {
-    console.log(event.type)
-  });
-
-  $(".new-tweet textarea").on("keypress", (event) => {
-    console.log(event.type)
-  });
-
-  $(".new-tweet textarea").on("input", (event) => {
-    console.log(event.type)
+  $(".new-tweet textarea").on("input", function() {
+    console.log($(this).val().length)
   });
 
 });
