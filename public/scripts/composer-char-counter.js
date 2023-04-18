1b233e7ddbf43f9833315612e6bbec2ade394ce6
Saim Amir <saimxamir@gmail.com>
2023-04-18 08:26:54 -0400

    remove console log code not req

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 
   $(".new-tweet textarea").on("input", function() {
-    console.log(140 - $(this).val().length);
-  });
-
-  $(".new-tweet textarea").on("input", function() {
     let counter = $(this).siblings(".new-tweet-footer").find(".counter");
     counter.text(140 - $(this).val().length);
   });
