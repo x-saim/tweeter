b35de091a99735b1cdece46fcb6f2b202d0d98f6
Saim Amir <saimxamir@gmail.com>
2023-04-18 08:41:23 -0400

    add JS to create invalid char class and style it to be red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 
   $(".new-tweet textarea").on("input", function() {
+    //traverse down DOM Tree
     let counter = $(this).siblings(".new-tweet-footer").find(".counter");
-    counter.text(140 - $(this).val().length);
+    let charCount = 140 - $(this).val().length
+
+    //set new value to counter text
+    counter.text(charCount);
+ 
+    //condition for turning char count red
+    if (charCount < 0) {
+      counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid");
+    }
+    
   });
 
 });
