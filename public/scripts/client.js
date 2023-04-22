edeb9862a4a5058758b27c5e2229c85fac9c9cc6
Saim Amir <saimxamir@gmail.com>
2023-04-22 17:59:41 -0400

    add hover CSS to icons and tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  console.log('ready');
+  $.getJSON("/home/labber/tweeter/server/data-files/initial-tweets.json", function(data) {
+    console.log(data);
+  });
+});
