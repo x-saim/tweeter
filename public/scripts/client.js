4412093f522f8c99004441404020e4d9d8ea3847
Saim Amir <saimxamir@gmail.com>
2023-04-22 21:36:03 -0400

    remove & add comments, add alt attribute, add moment.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,8 +5,7 @@
  */
 
 $(document).ready(() => {
-  console.log('ready');
-  const tweetData = 
+  const tweetData =
     {
       "user": {
         "name": "Newton",
@@ -17,60 +16,56 @@ $(document).ready(() => {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
       "created_at": 1682007053297
-    }
+    };
 
-const createTweetElement = (tweetObject) => {
+  const createTweetElement = (tweetObject) => {
+    const $userName = $(`<p id="user-name">${tweetObject["user"]["name"]}</p>`);
+    const $userHandle = $(`<a id="user-handle">${tweetObject["user"]["handle"]}</a>`);
+    const $avatars = $(`<img src="${tweetObject["user"]["avatars"]}" width="50" height="50">`);
+    const $text = $(`<p>${tweetObject["content"]["text"]}</p>`);
+    const $createdAt = $(`<p>${tweetObject["created_at"]}</p>`);
 
- // let relativeTime = moment(tweetObject["created_at"], "YYYYMMDD").fromNow();
+    //tweet container structure
+    const $tweet = $("<article>").addClass("tweet");
 
-  const $userName = $(`<p id="user-name">${tweetObject["user"]["name"]}</p>`);
-  const $userHandle = $(`<a id="user-handle">${tweetObject["user"]["handle"]}</a>`);
-  const $avatars = $(`<img src="${tweetObject["user"]["avatars"]}" width="50" height="50">`);
-  const $text = $(`<p>${tweetObject["content"]["text"]}</p>`);
-  const $createdAt = $(`<p>${tweetObject["created_at"]}</p>`);
+    //header structure
+    const $header = $("<header>");
+    const $userInfo = $("<div>").addClass("user-info");
+    const $userHandleCont = $("<div>").addClass("user-handle");
+    $header.append($userInfo).append($userHandleCont);
 
-  //tweet container structure
-  const $tweet = $("<article>").addClass("tweet");
+    //appending from tweet object
+    $userInfo.append($avatars);
+    $userInfo.append($userName);
+    $userHandleCont.append($userHandle);
 
-  //header structure
-  const $header = $("<header>");
-  const $userInfo = $("<div>").addClass("user-info");
-  const $userHandleCont = $("<div>").addClass("user-handle");
-  $header.append($userInfo).append($userHandleCont);
+    //content structure
+    const $content = $("<div>").addClass("content");
+    $content.append($text);
 
-  //appending from tweet object
-  $userInfo.append($avatars);
-  $userInfo.append($userName);
-  $userHandleCont.append($userHandle);
-
-  //content structure
-  const $content = $("<div>").addClass("content");
-  $content.append($text);
-
-  //footer structure
-  const $footer = $("<footer>");
-  const $time = $("<div>").addClass("time");
-  const $icons = $("<div>").addClass("icons");
-  const $flag = $("<a>").addClass("flag").html(`<i class="fa-solid fa-flag"></i>`);
-  const $retweet = $("<a>").addClass("retweet").html(`<i class="fa-solid fa-retweet"></i>`);
-  const $like = $("<a>").addClass("like").html(`<i class="fa-solid fa-heart"></i>`);
-  $icons.append($flag).append($retweet).append($like);
-  $time.append($createdAt);
-  $footer.append($time).append($icons);
+    //footer structure
+    const $footer = $("<footer>");
+    const $time = $("<div>").addClass("time");
+    const $icons = $("<div>").addClass("icons");
+    const $flag = $("<a>").addClass("flag").html(`<i class="fa-solid fa-flag"></i>`);
+    const $retweet = $("<a>").addClass("retweet").html(`<i class="fa-solid fa-retweet"></i>`);
+    const $like = $("<a>").addClass("like").html(`<i class="fa-solid fa-heart"></i>`);
+    $icons.append($flag).append($retweet).append($like);
+    $time.append($createdAt);
+    $footer.append($time).append($icons);
 
   
-  $tweet.append($header)
-  $tweet.append($content);
-  $tweet.append($footer);
+    $tweet.append($header);
+    $tweet.append($content);
+    $tweet.append($footer);
 
+    return $tweet;
+  };
 
-  return $tweet
-};
+  const $tweet = createTweetElement(tweetData);
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  });
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
 
