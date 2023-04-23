6d506606d67b2568f5dde91528804488e0074495
Saim Amir <saimxamir@gmail.com>
2023-04-22 20:36:19 -0400

    implementing createTweet Element function with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
 $(document).ready(() => {
   console.log('ready');
-  $.getJSON("/home/labber/tweeter/server/data-files/initial-tweets.json", function(data) {
-    console.log(data);
+  const tweetData = 
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1682007053297
+    }
+
+const createTweetElement = (tweetObject) => {
+  const $userName = $(`<p id="user-name">${tweetObject["user"]["name"]}</p>`);
+  const $userHandle = $(`<a id="user-handle">${tweetObject["user"]["handle"]}</a>`);
+  const $avatars = $(`<img src="${tweetObject["user"]["avatars"]}" width="50" height="50">`);
+  const $text = $(`<p>${tweetObject["content"]["text"]}</p>`);
+  const $createdAt = $(`<p>${tweetObject["created_at"]}</p>`);
+
+  const $tweet = $("<article>").addClass("tweet");
+  const $header = $("<header>");
+
+  //header structure
+  const $userInfo = $("<div>").addClass("user-info");
+  const $userHandleCont = $("<div>").addClass("user-handle");
+
+  $userInfo.append($avatars);
+  $userInfo.append($userName);
+  $userHandleCont.append($userHandle);
+
+  //content structure
+  const $content = $("<div>").addClass("content");
+  $content.append($text);
+
+  //footer structure
+  const $footer = $("<footer>");
+  const $time = $("<div>").addClass("time");
+  const $icons = $("<div>").addClass("icons");
+  const $flag = $("<a>").addClass("flag").html(`<i class="fa-solid fa-flag"></i>`);
+  const $retweet = $("<a>").addClass("retweet").html(`<i class="fa-solid fa-retweet"></i>`);
+  const $like = $("<a>").addClass("like").html(`<i class="fa-solid fa-heart"></i>`);
+
+  //content body
+
+  
+  $tweet.append($header)
+  $tweet.append($content);
+  $tweet.append($footer);
+
+
+  return $tweet
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   });
-});
+
