60e55befafd6a0b2117245a30fe99cbfe291aed1
Saim Amir <saimxamir@gmail.com>
2023-04-26 19:41:37 -0400

    create new stylesheets and create media query

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
-  // Scroll event for back to the top button
-  // Show the button when the user scrolls down 20px from the top of the document
+/**
+ * Attach a scroll event listener to the window object. If the current vertical scroll position of the window is greater than 20 pixels, display a button with the ID "topBtn" and hide the "nav" element. Otherwise, hide the "topBtn" button and show the "nav" element.
+ * 
+ */
   $(window).scroll(function() {
     if ($(this).scrollTop() > 20) {
       $('#topBtn').css("display","block");
