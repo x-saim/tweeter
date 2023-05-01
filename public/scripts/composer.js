a157ec302f54587eb7c461f5b1857186627b15be
Saim Amir <saimxamir@gmail.com>
2023-05-01 17:00:09 -0400

    set nav bar to be fixed and transparant upon scroll

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -7,18 +7,29 @@ $(document).ready(function() {
  */
   $(window).scroll(function() {
     if ($(this).scrollTop() > 20) {
-      $('#topBtn').css("display","block");
-      $("nav").css("display","none");
+      $('#topBtn').css("display","block"); //enables the navigation to the top buttom
+      $("nav").css("background-color","transparent") //turns nav background transparant on scroll
+      $(".new-tweet-header").hide(); //hides the compose block in nav on scroll
     } else {
       $('#topBtn').css("display","none");
-      $("nav").css("display","flex");
+      $("nav").css("background-color","#4056A1")
+      $(".new-tweet-header").show();
+
     }
   });
+
+
+  /*
+  Binds a click event to the #topBtn element that animates the page scroll to the top, slides down the .new-tweet element or tweet box element, focuses on the #tweet-text element, changes the background color of the <nav> element to be visible, shows a .new-tweet-header element, and returns false to prevent the default behavior.
+  */
 
   $("#topBtn").click(function() {
     $("html,body").animate({ scrollTop: 0 }, "slow");
     $(".new-tweet").slideDown();
     $("#tweet-text").focus();
+
+    $("nav").css("background-color","#4056A1")
+    $(".new-tweet-header").show();
     return false;
   });
 });
